2b2340e61acaa627c038e34751b5b0f95e6e93bf
Ali <alighorbani20002@gmail.com>
2020-05-20 10:22:41 +0430

    Auth Service Provider ACL

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Permission;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,18 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        if (Schema::hasTable('Permission')) {
+            $permissions = $this->getPermissions();
+            foreach ($permissions as $permission) {
+                Gate::define($permission->name, function ($user) use ($permission) {
+                    return $user->hasRole($permission->roles);
+                });
+            }
+        }
+    }
+
+    public function getPermissions()
+    {
+        return Permission::with('roles')->get();
     }
 }
